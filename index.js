cbd54676bfef1a2ea159de07fa1c1eff98838071
aditi <aditideshpande2908@gmail.com>
2025-08-14 11:47:20 +0530

    update operation & error handling'

--- index.js
+++ index.js
@@ -9,8 +9,73 @@ const PORT = process.env.PORT || 3001
 const app = express()
 app.use(express.json())
 app.use(cors())
-
 app.use(express.static('dist'))
+
+app.get('/', (req, res) => {
+	res.send('<h1>hello world</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+	Contact.find({}).then((contacts) => {
+		res.json(contacts)
+	})
+})
+
+app.get('/info', async (req, res) => {
+	const contactCount = await Contact.estimatedDocumentCount()
+	console.log('contact length', contactCount)
+	const currentDate = new Date()
+
+	res.send(
+		`<p>Phonebook has info for ${contactCount} people</p><p>${currentDate}</p>`
+	)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = req.params.id
+	Contact.findById(id).then((contact) => res.json(contact))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+	const id = req.params.id
+	Contact.findByIdAndDelete(id)
+		.then((contact) => {
+			res.status(204).end()
+		})
+		.catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+	const { id, name, number } = req.body
+	Contact.findById(id).then((contact) => {
+		if (!contact) {
+			return res.status(404).end()
+		}
+
+		contact.name = name
+		contact.number = number
+
+		return contact
+			.save()
+			.then((savedContact) => {
+				res.json(savedContact)
+			})
+			.catch((error) => next(error))
+	})
+})
+
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+	if (!body) {
+		return res.status(404).send('send appropriate information')
+	}
+
+	const newContact = new Contact({
+		name: body.name,
+		number: body.number,
+	})
+	newContact.save().then((savedContact) => res.json(savedContact))
+})
 
 const requestLogger = morgan((tokens, req, res) => {
 	return [
@@ -26,80 +91,14 @@ const requestLogger = morgan((tokens, req, res) => {
 
 app.use(requestLogger)
 
-let phonebook = [
-	{
-		id: '1',
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: '2',
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: '3',
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: '4',
-		name: 'Mary Poppendieck',
-		number: '39-23-6423122',
-	},
-]
-
-app.get('/', (req, res) => {
-	res.send('<h1>hello world</h1>')
-})
-
-app.get('/api/persons', (req, res) => {
-	Contact.find({}).then((contacts) => {
-		res.json(contacts)
-	})
-})
-
-// todo: get count to show info
-app.get('/info', (req, res) => {
-	const contactCount = phonebook.length
-	console.log('contact length', contactCount)
-	const currentDate = new Date()
-
-	res.send(
-		`<p>Phonebook has info for ${contactCount} people</p><p>${currentDate}</p>`
-	)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-	const id = req.params.id
-	Contact.findById(id).then((contact) => res.json(contact))
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-	const id = req.params.id
-	phonebook = phonebook.filter((person) => person.id !== id)
-
-	res.status(204).send('record deleted')
-})
-
-app.post('/api/persons', (req, res) => {
-	const body = req.body
-	if (!body) {
-		return res.status(404).send('send appropriate information')
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message)
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
 	}
 
-	/* 	Contact.findOne({ name: body.name }).then((result) => {
-		if (result) {
-			return res.status(404).send('name must be unique')
-		}
-	}) */
-
-	const newContact = new Contact({
-		name: body.name,
-		number: body.number,
-	})
-	newContact.save().then((savedContact) => res.json(savedContact))
-})
+	next(error)
+}
 
 app.listen(PORT, () => {
 	console.log('server running on port ', PORT)
